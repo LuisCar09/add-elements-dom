58af509b7212124c2659854c3e195d773c993053
Luis Carlos <luiscbravo94@gmail.com>
2024-06-11 21:29:25 +0100

    ready to play

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+const addButton = document.getElementById('agregar')
+const uNorderedList = document.getElementById('lista')
+
+addButton.addEventListener('click',() =>{
+    const item = prompt('Ingrese tarea pendiente')
+    const itemList = document.createElement('li')
+    if (item.length > 1) {
+        itemList.innerText = item
+        uNorderedList.appendChild(itemList)
+    }else{
+        alert('Debes ingresar una tarea')
+    }
+})
